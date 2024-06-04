b5065cd21c6f7f76ac4d09a9beb724528f6e9dc8
klu_2200031618 <kk258786@gmail.com>
2024-06-04 19:59:37 +0530

    Seperated within respective pages and need to complete the snake game logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './allPages.css'
-
+import {StartGame} from './startGame'
 function App() {
-
-  const handleStartGame=()=>{
-    document.getElementById('snake_game').className='snake-game'
-    document.getElementById('startgame_container').className='startgame-container-hidden'
-  }
-
+  
   return (
     <div className='main-container'>
-      <div className='startgame-container' id='startgame_container'>
-        <div><h1>About Game Picture and Some Gif</h1></div>
-        <button className='startgame-button' onClick={handleStartGame}>Start Game</button>
-      </div>
+      <StartGame />
       <div className='snake-game-hidden' id='snake_game'></div>
     </div>
   );
--- src/startGame.js
+++ src/startGame.js
@@ -1,7 +1,16 @@
-export const startGame=()=>{
+export const StartGame=()=>{
+
+    const handleStartGame=()=>{
+        document.getElementById('snake_game').className='snake-game'
+        document.getElementById('startgame_container').className='startgame-container-hidden'
+      }    
+
     return(
-        <div>
-            Hello from Start Game
+        <div className='startgame-container' id='startgame_container'>
+            <div>
+                <h1>About Game Picture and Some Gif</h1>
+            </div>
+            <button className='startgame-button' onClick={handleStartGame}>Start Game</button>
         </div>
     )
 }

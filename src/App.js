a9caa78056819acbd5c2c86c37b37debd0bca5ab
klu_2200031618 <kk258786@gmail.com>
2024-06-04 19:48:28 +0530

    StartGame logic has been completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './allPages.css'
 
 function App() {
+
+  const handleStartGame=()=>{
+    document.getElementById('snake_game').className='snake-game'
+    document.getElementById('startgame_container').className='startgame-container-hidden'
+  }
+
   return (
     <div className='main-container'>
-      <div className='snake'></div>
+      <div className='startgame-container' id='startgame_container'>
+        <div><h1>About Game Picture and Some Gif</h1></div>
+        <button className='startgame-button' onClick={handleStartGame}>Start Game</button>
+      </div>
+      <div className='snake-game-hidden' id='snake_game'></div>
     </div>
   );
 }
